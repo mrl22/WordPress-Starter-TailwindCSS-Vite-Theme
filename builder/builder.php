c32fc59792a9ee78b60e2ebac06a1c4535546544
Richard Leishman <richard@webfwd.co.uk>
2024-08-19 21:54:22 +0100

    Builder

---
+++ builder/builder.php
@@ -0,0 +1,18 @@
+<?php if (have_rows('builder')): ?>
+    <div id="builder">
+        <?php
+        $builder = 0;
+        while (have_rows('builder')): the_row();
+            $builder++;
+            $layout = esc_attr(get_row_layout());
+            $layout_path = get_template_directory() . '/builder/_' . $layout;
+            ?>
+            <section class="<?php echo $layout; ?> builder<?php echo $builder; ?>">
+                <?php
+                if (file_exists($layout_path . '.php')) require($layout_path);
+                else echo '<strong>[builder error: Block "_' . $layout . '.php" could not be found]</strong>';
+                ?>
+            </section>
+        <?php endwhile; ?>
+    </div>
+<?php endif; ?>
